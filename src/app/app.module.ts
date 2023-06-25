526ac360e7eed121a4447e0142844ac073978db5
DAW2T6GN <DAW2T6GN@gmail.com>
2023-06-25 15:10:09 -0500

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './soluciones/home/home.component';
+import { MaterialModule } from './angular-material/material.module';
+import { RouterModule } from '@angular/router';
+import { SolucionesComponent } from './soluciones/soluciones.component';
+import { PromedioComponent } from './soluciones/promedio/promedio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SolucionesComponent,
+    PromedioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    RouterModule.forRoot([
+      {path: 'soluciones', component: SolucionesComponent,
+      children: [
+        {path: 'home', component: HomeComponent},
+        {path: 'promedio', component:PromedioComponent}]
+      
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
